e6b63f9a5530821e470cb97359255ccb3fdc0264
YedixonRDev <yedixonjrf@gmail.com>
2024-03-30 00:30:00 -0500

    box style

--- components/indexBoxTable.php
+++ components/indexBoxTable.php
@@ -7,10 +7,13 @@ class BoxTable {
         $this->num = $Nun;
         $this->onClick = $Function;
 
-        echo '<div class="Table" onclick="' . $this->onClick . '">
+        echo '
+        <div class="container-mesa">
+            <div class="Table" onclick="' . $this->onClick . '">
                 <img src="./assets/imgs/table.png" alt="mesa1">
                 Mesa ' . $this->num . '
-            </div>';
+            </div>
+        </div>';
     }
 }
 
