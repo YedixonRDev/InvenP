6ccefdceb29d39d19633bf8595cc693ee755beab
YedixonRDev <yedixonjrf@gmail.com>
2024-04-10 23:00:25 -0500

    .

--- core/app.php
+++ core/app.php
@@ -1,68 +1,72 @@
 <?php
-    class AppRoutes {
-        private $routes = array();
-        public  function addRoutes($method, $path, $callback) {
-            $this->routes[] = array(
-                'method' => $method,
-                'path' => $path,
-                'callback' => $callback
-            );
-        }
-        public  function getRoutes() {
-            return $this->routes;
-        }
+class AppRoutes
+{
+    private $routes = array();
+    public  function addRoutes($method, $path, $callback)
+    {
+        $this->routes[] = array(
+            'method' => $method,
+            'path' => $path,
+            'callback' => $callback
+        );
+    }
+    public  function getRoutes()
+    {
+        return $this->routes;
+    }
+}
+
+class AppViews
+{
+
+    private $routes;
+    public function __construct($listRoutes)
+    {
+        $this->routes = $listRoutes;
     }
 
-    class AppViews {
-
-        private $routes;
-        public function __construct($listRoutes) {
-            $this->routes = $listRoutes;
+    public function loadViews()
+    {
+        $requestMethod = $_SERVER['REQUEST_METHOD'];
+        if (isset($_GET['url'])) {
+            $requestPath = $_GET['url'];
+        } else {
+            $requestPath = '/';
         }
-
-        public function loadViews() {
-            $requestMethod = $_SERVER['REQUEST_METHOD'];
-            if (isset($_GET['url'])) {
-                $requestPath = $_GET['url'];
-            }else {
-                $requestPath = '/';
+        foreach ($this->routes as $route) {
+            if ($requestMethod === $route['method'] && $requestPath === $route['path']) {
+                call_user_func($route['callback']);
+                return;
             }
-            foreach ($this->routes as $route) {
-                if ($requestMethod === $route['method'] && $requestPath === $route['path']) {
-                    call_user_func($route['callback']);
-                    return;
-                }
-
-            }  
-            echo 'Error 404'; 
         }
-
+        echo 'Error 404';
     }
+}
 
-    class AppScript {
-        private $routes;
-        public function __construct($listRoutes) {
-            $this->routes = $listRoutes;
+class AppScript
+{
+    private $routes;
+    public function __construct($listRoutes)
+    {
+        $this->routes = $listRoutes;
+    }
+    public function loadScript()
+    {
+        $requestMethod = $_SERVER['REQUEST_METHOD'];
+        if (isset($_GET['url'])) {
+            $requestPath = $_GET['url'];
+        } else {
+            $requestPath = '/';
         }
-        public function loadScript() {
-            $requestMethod = $_SERVER['REQUEST_METHOD'];
-            if (isset($_GET['url'])) {
-                $requestPath = $_GET['url'];
-            }else {
-                $requestPath = '/';
+        foreach ($this->routes as $route) {
+            if ($requestMethod === $route['method'] && $requestPath === $route['path']) {
+                if ($route['path'] == '/') {
+                    echo '<script src="scripts/script.index.js"></script>';
+                } else {
+                    echo '<script src="scripts/script.' . $route['path'] . '.js"></script>';
+                }
+                return;
             }
-            foreach ($this->routes as $route) {
-                if ($requestMethod === $route['method'] && $requestPath === $route['path']) {
-                    if ($route['path']=='/') {
-                        echo '<script src="scripts/script.index.js"></script>';
-                    }else {
-                        echo '<script src="scripts/script.'.$route['path'].'.js"></script>';
-                    }
-                     return;
-                }
-            }   
         }
     }
-
-?>
-
+}
